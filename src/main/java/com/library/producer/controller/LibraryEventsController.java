4e8bebd75d83ac81fddb3f5c240371cf0e7bba31
Arthur Marques <arthur.l2@hotmail.com>
2022-02-22 22:09:29 -0300

    configuration

--- src/main/java/com/library/producer/controller/LibraryEventsController.java
+++ src/main/java/com/library/producer/controller/LibraryEventsController.java
@@ -6,13 +6,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.library.producer.domain.LibraryEvent;
+import com.library.producer.producer.LibraryEventProducer;
 
 @RestController
 public class LibraryEventsController {
 
+	private LibraryEventProducer libraryEventProducer;
+	
+	public LibraryEventsController(LibraryEventProducer libraryEventProducer) {
+		this.libraryEventProducer = libraryEventProducer;
+	}
+
 	@PostMapping("/v1/libraryevent")
-	public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+	public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+		libraryEventProducer.sendLibraryEvent(libraryEvent);
 		return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
 	}
 }
